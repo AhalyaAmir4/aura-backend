e99f9fcbdf80ad964101b72f514ecb288109d44f
unknown <ahal.amir2005@gmail.com>
2026-05-18 20:04:19 +0530

    removed placeholder

--- src/main/java/com/aura/attendance/dto/_Placeholder.java
+++
@@ -1,9 +0,0 @@
-package com.aura.attendance.dto;
-
-import com.aura.attendance.entity.User;
-import lombok.*;
-
-// ========= AUTH DTOs =========
-
-@Data @NoArgsConstructor @AllArgsConstructor
-class _Placeholder {} // keeps IDE happy
